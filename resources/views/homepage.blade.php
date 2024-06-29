997269a5285d39f1f2ee4033c1bcca3708c7f9a2
NicholasBuiatti <nic.buiatti@gmail.com>
2024-06-29 11:07:37 +0200

    creazioen layout e rotta

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,7 +7,9 @@
     <title>Laravel-Comics</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
-    
+<body class="bg-dark text-white">
+    <section class="container">
+        <h1 class="text-center text-danger">LARAVEL COMICS HOMEPAGE</h1>
+    </section>
 </body>
 </html>
